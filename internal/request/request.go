86c82809d078763c361969378c03d2b395a89b92
Tian Istenič <tian.istenic34@gmail.com>
2026-04-09 10:10:41 +0200

    feat: implemented another parse function to parse instead of RequestFromReader

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,6 +1,7 @@
 package request
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"strings"
@@ -16,7 +17,40 @@ type Request struct {
 	ParserState int
 }
 
-// func (r *Request) parse(data []byte) (int, error)
+const (
+	parserStateRequestLine = iota
+	parserStateDone
+)
+
+func (request *Request) parse(data []byte) (int, error) {
+	switch request.ParserState {
+	case parserStateRequestLine:
+		i := bytes.Index(data, []byte("\r\n"))
+		if i == -1 {
+			return 0, nil
+		}
+		line := string(data[:i])
+		parts := strings.Split(line, " ")
+		if len(parts) != 3 {
+			return 0, fmt.Errorf("invalid request line: expected 3 parts, got %d", len(parts))
+		}
+		if !strings.HasPrefix(parts[2], "HTTP/") {
+			return 0, fmt.Errorf("invalid http version: %s", parts[2])
+		}
+
+		request.RequestLine.Method = parts[0]
+		request.RequestLine.RequestTarget = parts[1]
+		request.RequestLine.HttpVersion = strings.TrimPrefix(parts[2], "HTTP/")
+		request.ParserState = parserStateDone
+
+		return i + 2, nil
+	case parserStateDone:
+		return 0, nil
+	default:
+		return 0, fmt.Errorf("unknown parser state: %d", request.ParserState)
+	}
+
+}
 
 func RequestFromReader(reader io.Reader) (*Request, int, error) {
 	file, err := io.ReadAll(reader)
